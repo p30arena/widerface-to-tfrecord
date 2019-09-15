b48e707f938144271b130e5bab740da64698ca46
Ali EP <p30arena@gmail.com>
2019-09-15 21:18:02 +0430

    Update wider_to_tfrecord.py

--- wider_to_tfrecord.py
+++ wider_to_tfrecord.py
@@ -3,6 +3,7 @@ import numpy
 import cv2
 import os
 import hashlib
+import io
 
 import config
 from utils import dataset_util
@@ -36,9 +37,15 @@ def parse_test_example(f, images_path):
   filepath = os.path.join(images_path, filename)
 
   image_raw = cv2.imread(filepath)
+  if config.RESIZE:
+    image_raw = cv2.resize(image_raw, (config.RESIZE, config.RESIZE))
 
-  encoded_image_data = open(filepath, "rb").read()
-  key = hashlib.sha256(encoded_image_data).hexdigest()
+  is_success, buffer = cv2.imencode(".jpg", image_raw)
+  encoded_image_data = buffer.tobytes()
+  # encoded_image_data = io.BytesIO(buffer)
+  # encoded_image_data = open(filepath, "rb").read()
+  # key = hashlib.sha256(encoded_image_data).hexdigest()
+  key = ''
 
   height, width, channel = image_raw.shape
 
@@ -49,6 +56,8 @@ def parse_test_example(f, images_path):
     'image/source_id': dataset_util.bytes_feature(filename.encode('utf-8')),
     'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
     'image/encoded': dataset_util.bytes_feature(encoded_image_data),
+    # 'image/array': dataset_util.float_list_feature(
+    #   (cv2.cvtColor(image_raw, cv2.COLOR_BGR2RGB) / 255.).flatten().tolist()),
     'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
   }))
 
@@ -71,19 +80,15 @@ def parse_example(f, images_path):
   poses = []
   truncated = []
   difficult_obj = []
+  raw_all_annot = []
 
   filename = f.readline().rstrip()
   if not filename:
     raise FileNameIsNone()
 
   filepath = os.path.join(images_path, filename)
-
-  image_raw = cv2.imread(filepath)
-
-  encoded_image_data = open(filepath, "rb").read()
-  key = hashlib.sha256(encoded_image_data).hexdigest()
-
-  height, width, channel = image_raw.shape
+  if os.path.isfile(filepath) == False:
+    raise IOError()
 
   face_num = int(f.readline().rstrip())
   if not face_num:
@@ -94,20 +99,83 @@ def parse_example(f, images_path):
     if not annot:
       raise Exception()
 
+    raw_all_annot.append(annot)
+
+  image_raw = cv2.imread(filepath)
+  if image_raw is None:
+    raise IOError()
+  original_height, original_width, original_channel = image_raw.shape
+  # aspect_ratio = original_width / original_height
+  # if aspect_ratio < .9 or aspect_ratio > 1.1:
+  #   # image looses too much info if not square cropped
+  #   bg_i = -1  # biggest
+  #   bg_wh = 0
+  #   for i in range(len(raw_all_annot)):
+  #     annot = raw_all_annot[i]
+  #     if float(annot[2]) > 25.0 and float(annot[3]) > 30.0:
+  #       sum = float(annot[2]) + float(annot[3])
+  #       if sum > bg_wh:
+  #         bg_i = i
+  #         bg_wh = sum
+  #
+  #   bg_annot = raw_all_annot[bg_i]
+  #   bg_box_center = (float(bg_annot[0]) + float(bg_annot[2]) / 2, float(bg_annot[1]) + float(bg_annot[3]) / 2)
+  #   min_d_start = min(bg_box_center[0], bg_box_center[1])
+  #   min_d = min_d_start
+  #   dx_end = original_width - bg_box_center[0]
+  #   dy_end = original_height - bg_box_center[1]
+  #   min_d_end = min(dx_end, dy_end)
+  #
+  #   if min_d_end < min_d_start:
+  #     min_d = min_d_end
+  #
+  #   new_x_axis = bg_box_center[0] - min_d
+  #   new_y_axis = bg_box_center[1] - min_d
+  #   new_w = bg_box_center[0] + min_d
+  #   new_h = bg_box_center[1] + min_d
+  #   image_raw = image_raw[new_y_axis:new_h, new_x_axis:new_w]
+  #   raw_all_annot = [
+  #     annot for annot in raw_all_annot if
+  #     float(bg_annot[0]) + float(bg_annot[2]) <= new_w and
+  #     float(bg_annot[1]) + float(bg_annot[3]) <= new_h
+  #   ]
+  #   raw_all_annot = [
+  #     [float(annot[0]) - new_x_axis, float(annot[1]) - new_y_axis, annot[2], annot[3]] for annot in raw_all_annot
+  #   ]
+
+  if config.RESIZE:
+    image_raw = cv2.resize(image_raw, (config.RESIZE, config.RESIZE))
+
+  is_success, buffer = cv2.imencode(".jpg", image_raw)
+  encoded_image_data = buffer.tobytes()
+  # encoded_image_data = io.BytesIO(buffer)
+  # encoded_image_data = open(filepath, "rb").read()
+  # key = hashlib.sha256(encoded_image_data).hexdigest()
+  key = ''
+
+  height, width, channel = image_raw.shape
+
+  scaleW = width / original_width
+  scaleH = height / original_height
+
+  for i in range(len(raw_all_annot)):
+    annot = raw_all_annot[i]
+
     # WIDER FACE DATASET CONTAINS SOME ANNOTATIONS WHAT EXCEEDS THE IMAGE BOUNDARY
-    if (float(annot[2]) > 25.0):
-      if (float(annot[3]) > 30.0):
-        xmins.append(max(0.005, (float(annot[0]) / width)))
-        ymins.append(max(0.005, (float(annot[1]) / height)))
-        xmaxs.append(min(0.995, ((float(annot[0]) + float(annot[2])) / width)))
-        ymaxs.append(min(0.995, ((float(annot[1]) + float(annot[3])) / height)))
-        classes_text.append(b'face')
-        classes.append(1)
-        poses.append("front".encode('utf8'))
-        truncated.append(int(0))
+    if float(annot[2]) > 25.0 and float(annot[3]) > 30.0:
+      xmins.append(max(0.005, (float(annot[0]) * scaleW) / width))
+      ymins.append(max(0.005, (float(annot[1]) * scaleH) / height))
+      xmaxs.append(min(0.995, ((float(annot[0]) + float(annot[2])) * scaleW) / width))
+      ymaxs.append(min(0.995, ((float(annot[1]) + float(annot[3])) * scaleH) / height))
+      classes_text.append(b'face')
+      classes.append(1)
+      poses.append("front".encode('utf8'))
+      truncated.append(int(0))
 
   if len(classes) == 0:
     return None
+
+  to_str = lambda l: (str(x) for x in l)
 
   tf_example = tf.train.Example(features=tf.train.Features(feature={
     'image/height': dataset_util.int64_feature(int(height)),
@@ -116,7 +184,12 @@ def parse_example(f, images_path):
     'image/source_id': dataset_util.bytes_feature(filename.encode('utf-8')),
     'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
     'image/encoded': dataset_util.bytes_feature(encoded_image_data),
+    # 'image/array': dataset_util.float_list_feature(
+    #   (cv2.cvtColor(image_raw, cv2.COLOR_BGR2RGB) / 255.).flatten().tolist()),
     'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
+    'image/object/bbox/encoded': dataset_util.bytes_feature(
+      ','.join(to_str(xmins + xmaxs + ymins + ymaxs)).encode('utf-8')
+    ),
     'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
     'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
     'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
